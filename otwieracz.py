3ee4092ba47a3c1fbb67549121273fddcbc11a5d
jwadamiak <james.adamiak@gmail.com>
2016-12-09 17:38:20 +0100

    Add files via upload
    09/12/2016
    podział na pliki

---
+++ otwieracz.py
@@ -0,0 +1,24 @@
+# otwiera pliki ze slowami kluczowymi i wrzuca te slowa w
+# tablice
+
+def potnijPlik(plik):
+    wynik = ""
+    with open(plik, 'r') as f:  # plik z przedmiotami
+        for line in f:
+            for word in line.split():
+                wynik += word
+                wynik += " "
+                pociete = wynik.split(" ")
+    return pociete
+
+#tnie obojetnie jaki plik
+def potnij(text):
+    pociete = text.split(" ")
+    return pociete
+
+# wrzuca do tablicy miejsca w magazynie (klucze)
+def klucze(slownik):
+    t = []
+    for k in slownik:
+        t.append(k)
+    return t
---
+++ szukacz.py
@@ -0,0 +1,36 @@
+# wyszukuje polecenia oraz przedmioty z rozkazu wpisanego przez uzytkownika
+def przeszukujPolecenie(polecenie, komenda):  # przeszukujemy polecenia, zwracamy i usuwamy je
+    i = 0
+    j = 0
+    s = []
+    for i in range(len(komenda)):
+        for j in range(len(polecenie)):  # szukanie polecenia do pierwszego wystapienia
+            if komenda[i] == polecenie[j]:
+                s.append(komenda[i])
+    return s
+
+# tutaj udało się dać "z" jako parametr
+def przeszukujMiejsceSkad(magazyn, komenda, slowo = None):
+    for i in range(len(komenda)):
+        if (komenda[i] == slowo):
+            for j in range(i, len(komenda)):
+                for k in range(len(magazyn)):
+                    if komenda[j] == magazyn[k]:
+                        return komenda[j]
+    return 0
+
+# tutaj jak zrobie tak jak powyżej (dodatkowy parametr - słowo) i dam ("na" or "do") jako parametr, to nie widzi "do"
+# wiec zostawilem poki co tak jak jest, ale poszukam rozwiazania bo pewnie jakos sie da to zrobic
+def przeszukujMiejsceDokad(magazyn, komenda):
+    for i in range(len(komenda)):
+        if (komenda[i] == ("na" or "do")):
+            for j in range(i, len(komenda)):
+                for k in range(len(magazyn)):
+                    if komenda[j] == magazyn[k]:
+                        return komenda[j]
+    return 0
+
+""""
+zmienic na "jezeli znajdziemy przemiot w danym miejscu,
+["z", lokalizacja] jako parametr,
+"""

0f78269775c104eb551c5a055a677a04d9165895
MikolajSzumigalski <m.szumigalski@op.pl>
2017-01-14 20:48:39 +0100

    Update magazyn.py

--- magazyn.py
+++ magazyn.py
@@ -1,8 +1,8 @@
 # magazyn jako slownik z kluczami 4x9
-miejsca = {'a1': "beczka", 'a2': 0, 'a3': "beczka", 'a4': 0, 'a5': 0, 'a6': 0, 'a7': 0, 'a8': 0, 'a9': 0,
-            'b1': 0, 'b2': 0, 'b3': 0, 'b4': 0, 'b5': "beczka", 'b6': 0, 'b7': 0, 'b8': 0, 'b9': 0,
-            'c1': 0, 'c2': 0, 'c3': 0, 'c4': "puszka", 'c5': 0, 'c6': 0, 'c7': 0, 'c8': 0, 'c9': 0,
-            'd1': 0, 'd2': 0, 'd3': 0, 'd4': 0, 'd5': 0, 'd6': 0, 'd7': 0, 'd8': 0, 'd9': "beczka"}
+miejsca = {'a1': "beczka", 'a2': 0, 'a3': "beczka", 'a4': 0, 'a5': "pudelko", 'a6': 0, 'a7': 0, 'a8': "pudelko", 'a9': 0,
+            'b1': 0, 'b2': 0, 'b3': 0, 'b4': 0, 'b5': "beczka", 'b6': 0, 'b7': "puszka", 'b8': 0, 'b9': "opona",
+            'c1': 0, 'c2': 0, 'c3': 0, 'c4': "puszka", 'c5': 0, 'c6': "butelka", 'c7': 0, 'c8': 0, 'c9': 0,
+            'd1': "butelka", 'd2': "butelka", 'd3': 0, 'd4': "opona", 'd5': 0, 'd6': 0, 'd7': 0, 'd8': 0, 'd9': "beczka"}
 
 #wrzuca 0 tam gdzie puste miejsce i 1 tam gdzie zajete w magazynie po kolei do tablicy, 4x9=36 miejsc w tablicy
 tab_miejsca = []
@@ -12,6 +12,12 @@ def miejsca_do_tablicy(magazyn):
             tab_miejsca.append("beczka")
         elif magazyn[key] == "puszka":
             tab_miejsca.append("puszka")
+        elif magazyn[key] == "opona":
+            tab_miejsca.append("opona")
+        elif magazyn[key] == "butelka":
+            tab_miejsca.append("butelka")
+        elif magazyn[key] == "pudelko":
+            tab_miejsca.append("pudelko")
         else:
             tab_miejsca.append(0)
 miejsca_do_tablicy(miejsca)

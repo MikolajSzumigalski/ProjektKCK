ae3cc4d3dd211a396c18a73855a0149d242537c0
MikolajSzumigalski <m.szumigalski@op.pl>
2017-01-14 20:31:19 +0100

    Update magazyn.py

--- magazyn.py
+++ magazyn.py
@@ -1,17 +1,17 @@
 # magazyn jako slownik z kluczami 4x9
-miejsca = {'a1': "b", 'a2': 0, 'a3': "b", 'a4': 0, 'a5': 0, 'a6': 0, 'a7': 0, 'a8': 0, 'a9': 0,
-            'b1': 0, 'b2': 0, 'b3': 0, 'b4': 0, 'b5': "b", 'b6': 0, 'b7': 0, 'b8': 0, 'b9': 0,
-            'c1': 0, 'c2': 0, 'c3': 0, 'c4': "c", 'c5': 0, 'c6': 0, 'c7': 0, 'c8': 0, 'c9': 0,
-            'd1': 0, 'd2': 0, 'd3': 0, 'd4': 0, 'd5': 0, 'd6': 0, 'd7': 0, 'd8': 0, 'd9': "b"}
+miejsca = {'a1': "beczka", 'a2': 0, 'a3': "beczka", 'a4': 0, 'a5': 0, 'a6': 0, 'a7': 0, 'a8': 0, 'a9': 0,
+            'b1': 0, 'b2': 0, 'b3': 0, 'b4': 0, 'b5': "beczka", 'b6': 0, 'b7': 0, 'b8': 0, 'b9': 0,
+            'c1': 0, 'c2': 0, 'c3': 0, 'c4': "puszka", 'c5': 0, 'c6': 0, 'c7': 0, 'c8': 0, 'c9': 0,
+            'd1': 0, 'd2': 0, 'd3': 0, 'd4': 0, 'd5': 0, 'd6': 0, 'd7': 0, 'd8': 0, 'd9': "beczka"}
 
 #wrzuca 0 tam gdzie puste miejsce i 1 tam gdzie zajete w magazynie po kolei do tablicy, 4x9=36 miejsc w tablicy
 tab_miejsca = []
 def miejsca_do_tablicy(magazyn):
     for key in sorted(magazyn):
-        if magazyn[key] == "b":
-            tab_miejsca.append("b")
-        elif magazyn[key] == "c":
-            tab_miejsca.append("c")
+        if magazyn[key] == "beczka":
+            tab_miejsca.append("beczka")
+        elif magazyn[key] == "puszka":
+            tab_miejsca.append("puszka")
         else:
             tab_miejsca.append(0)
 miejsca_do_tablicy(miejsca)
@@ -63,7 +63,9 @@ def wypisz(stan_magazynu):
 #przemieszczamy przedmiot tylko wtedy, gdy:
 #1. mamy podane 'skąd' i 'cel' oraz wózek jest wolny; przedmiot znajduje się w miejscu 'skąd' a miejsce 'cel' jest puste
 #2. kiedy nie mamy podane 'skąd', ale mamy podany 'cel' i na wózku znajduje się towar
-def przemiesc(skad=None, cel=None):
+
+
+def przemiesc(skad=None, cel=None, przedmiot=None):
 
         if skad != None:
             if (miejsca[(skad)] != 0): #jesli na wskazanym miejscu w magazynie "skąd" jest przedmiot, to przenosimy we wskazane miejsce
